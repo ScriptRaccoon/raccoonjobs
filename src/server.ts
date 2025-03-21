41695f23697a0b8a595f6236b8d2fdef93c2b864
Script Raccoon <scriptraccoon@gmail.com>
2025-03-21 11:31:15 +0100

    add rate limit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { apiRouter } from './routers/api'
-
+import { rateLimiter } from './middlewares/rateLimit'
 import { config } from 'dotenv'
 config()
 
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
 	console.info(`Server is running on port ${PORT}`)
 })
+
+if (process.env.RATE_LIMIT === 'true') {
+	app.use(rateLimiter)
+}
 
 app.get('/health', (_req, res) => {
 	res.send('Server is running')
